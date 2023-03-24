8f7b838cc505fc320a21ae3ba4530866283a345e
Pranjal999999999 <pranjal9999999999@gmail.com>
2023-03-24 11:53:05 +0530

    Level 1 security

--- app.js
+++ app.js
@@ -3,13 +3,57 @@ const express=require("express");
 const app=express();
 const ejs=require("ejs");
 const bodyParser=require("body-parser");
+const mongoose=require("mongoose");
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://127.0.0.1:27017/userDB")
+.catch(function(err){
+    console.log(err);
+});
+const userSchema={
+    email:String,
+    password:String
+};
+const User=mongoose.model("User",userSchema);
 
 app.get("/",function(req,res){
+    res.render("home");
 
 });
+app.get("/login",function(req,res){
+    res.render("login");
+
+});
+app.post("/login",function(req,res){
+    User.findOne({email:req.body.username,password:req.body.password})
+    .then(function(result){
+        console.log("User logged in successfully");
+        console.log(result);
+        res.render("secrets");
+
+    })
+    .catch(function(err){
+        console.log(err);
+    });
+})
+app.get("/register",function(req,res){
+    res.render("register");
+    
+
+});
+app.post("/register",function(req,res){
+    const user=new User({email:req.body.username, password:req.body.password});
+    user.save()
+    .then(function(){
+        res.render("secrets");
+
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+
+})
 app.listen(3000,function(req,res){
     console.log("Server started on port 3000.");
 });
